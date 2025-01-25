12bba1915fc6e386a56e7ead59dea2de0983e80f
Oneven <71475028+OnevenL@users.noreply.github.com>
2025-01-25 22:05:56 +0800

    Update run_mimic_cxr.sh

--- run_mimic_cxr.sh
+++ run_mimic_cxr.sh
@@ -6,8 +6,8 @@ date=$(date '+%Y%m%d%H%M%S')
 
 python -m torch.distributed.run --nproc_per_node=4 --master_addr="localhost" --master_port=29503 report_generation.py \
        --output_dir=${result_dir}${date} \
-       --mage_dir="./data/mimic_cxr/images" \
-       --image_dir="./data/mimic_cxr/annotation.json" \
+       --image_dir="./data/mimic_cxr/images" \
+       --ann_path="./data/mimic_cxr/annotation.json" \
        --dataset_name="mimic_cxr" \
        --max_seq_length=100 \
        --threshold=10 \
