bec3c426e5c306330feabe3d23628503f17ecc8d
Oneven <71475028+OnevenL@users.noreply.github.com>
2025-01-25 22:10:33 +0800

    Update ahp.py

--- models/ahp.py
+++ models/ahp.py
@@ -40,8 +40,6 @@ class AHP_Decoder(nn.Module):
         """
         Args:
             med_config (str): path for the mixture of encoder-decoder model's configuration file
-            image_size (int): input image size
-            vit (str): model size of vision transformer
         """
         super().__init__()
         self.args = args
@@ -435,23 +433,6 @@ def init_tokenizer():
     tokenizer.add_special_tokens({'additional_special_tokens':['[ENC]']})
     tokenizer.enc_token_id = tokenizer.additional_special_tokens_ids[0]
     return tokenizer
-
-
-def create_vit(vit, image_size, use_grad_checkpointing=False, ckpt_layer=0, drop_path_rate=0):
-
-    assert vit in ['base', 'large'], "vit parameter must be base or large"
-    if vit=='base':
-        vision_width = 768
-        visual_encoder = VisionTransformer(img_size=image_size, patch_size=16, embed_dim=vision_width, depth=12,
-                                           num_heads=12, use_grad_checkpointing=use_grad_checkpointing, ckpt_layer=ckpt_layer,
-                                           drop_path_rate=0 or drop_path_rate)
-    elif vit=='large':
-        vision_width = 1024
-        visual_encoder = VisionTransformer(img_size=image_size, patch_size=16, embed_dim=vision_width, depth=24,
-                                           num_heads=16, use_grad_checkpointing=use_grad_checkpointing, ckpt_layer=ckpt_layer,
-                                           drop_path_rate=0.1 or drop_path_rate)
-    return visual_encoder, vision_width
-
 
 def is_url(url_or_filename):
     parsed = urlparse(url_or_filename)
